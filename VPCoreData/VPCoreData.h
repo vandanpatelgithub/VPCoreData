cf36fb5f3c0c89d124ef4e964a3615aae03a1837
Vandan Patel <vandan.ets@gmail.com>
2019-03-15 19:38:09 -0400

    Initial Commit

---
+++ VPCoreData/VPCoreData.h
@@ -0,0 +1,19 @@
+//
+//  VPCoreData.h
+//  VPCoreData
+//
+//  Created by Vandan Patel on 3/15/19.
+//  Copyright © 2019 Vandan Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VPCoreData.
+FOUNDATION_EXPORT double VPCoreDataVersionNumber;
+
+//! Project version string for VPCoreData.
+FOUNDATION_EXPORT const unsigned char VPCoreDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VPCoreData/PublicHeader.h>
+
+
